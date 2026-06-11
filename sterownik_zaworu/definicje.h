303e2b0b083413cb0632983b7d1f8b0d40ed0aa8
v33r005 <125038833+v33r005@users.noreply.github.com>
2026-06-11 12:20:30 +0200

    Update definicje.h

--- sterownik_zaworu/definicje.h
+++ sterownik_zaworu/definicje.h
@@ -1,7 +1,6 @@
 #pragma once
 
 inline constexpr char *SOFT_VERSION = "8.06.2026 @veeroos";
-constexpr uint8_t STATUS_LED_GPIO = 8;
+constexpr uint8_t STATUS_LED_GPIO = 10;
 constexpr uint8_t BUTTON_CFG_RELAY_GPIO = 5;
 constexpr uint8_t RELAY_GPIO = 4;
-
